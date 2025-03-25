78f2205d9f62b7ac88c815ab0c4612ee1ff83b07
revathi.kaley <revathi.kaley@bt.com>
2025-03-25 18:55:19 +0530

    Inherence push

--- pythonbasics/child_import.py
+++ pythonbasics/child_import.py
@@ -1,1 +1,16 @@
 # acquiring properties from parent to child
+
+from OOPS import calculator
+
+class child_import(calculator):
+    num2 =200
+
+    def __init__(self):
+        calculator.__init__(self, 2, 10)
+
+    def getCompleteData(self):
+        return self.num2 + self.num +self.summation()
+        
+
+obj =child_import()
+print(obj.getCompleteData())
